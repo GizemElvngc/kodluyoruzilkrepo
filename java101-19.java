4ffdd3ffe885d60384bdf5e60a31f1ed4ae4fbc7
GizemElvngc <73352461+GizemElvngc@users.noreply.github.com>
2021-06-21 00:05:08 +0300

    Update java101-19.java

--- java101-19.java
+++ java101-19.java
@@ -10,14 +10,13 @@ import java.util.Scanner;
 public class Baslangic {
     public static void main(String[] args) {
 
-        int n,k;
+        int n,k,i ,total =1;
 
         Scanner inp = new Scanner(System.in);
         System.out.println("Üssü alinacak sayi:");
         n = inp.nextInt();
         System.out.println("Üs olacak sayi:");
         k = inp.nextInt();
-        int i ,total =1;
         for( i=0 ; i<k ;i++){
 
          total *=n;
